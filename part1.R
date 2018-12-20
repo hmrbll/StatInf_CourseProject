ce5d4208caa0f185301481b4983964cf9abae984
hmrbll <huseyinemre_balli@hotmail.com>
2018-12-20 21:18:09 +0300

    Part I Done

--- part1.R
+++ part1.R
@@ -9,16 +9,17 @@ simulation <- replicate(sim_count, rexp(n, lambda))
 means <- apply(simulation, 2, mean)
 # Take a quick look of means
 range(means)
+means <- data.frame(means)
 library(ggplot2)
-plot_means <- ggplot() +
-        geom_histogram(aes(means), color = "black", fill = "cyan", binwidth = 0.25) +
+plot_means <- ggplot(means, aes(means)) +
+        geom_histogram(aes(y = ..density..), color = "black", fill = "cyan", binwidth = 0.25) +
                 labs(x = "Simulation means", y = "Frequency", title = "Histogram of Exponential Simulation Means") +
                         theme(plot.title = element_text(hjust = 0.5))
 plot_means
 # Question 1 - Sample Mean vs Theoretical Mean
 # Calculate theoretical and sample means
 theory_mean <- 1 / lambda
-sample_mean <- mean(means)
+sample_mean <- mean(means$means)
 # Compare them and plot
 abs(theory_mean - sample_mean)
 plot_means +
@@ -27,5 +28,13 @@ plot_means +
 # Question 2 - Sample Variance vs Theoretical Variance
 # Calculate theoretical and sample variances
 theory_variance <- (1 / lambda) ^ 2 / n
-sample_variance <- var(means)
+sample_variance <- var(means$means)
 data.frame(Sample.Variance = sample_variance, Theoretical.Variance = theory_variance, Difference = theory_variance - sample_variance)
+# Question 3 - Distribution
+# Graph of means' distribution
+plot_distribution <- plot_means +
+        stat_density(geom = "line", color = "red", lwd = 2)
+plot_distribution
+#Add normal distribution line for comparison
+plot_distribution +
+        stat_function(fun = dnorm, args = list(mean = theory_mean, sd = 1 / (lambda * sqrt(n))), color = "black", lwd = 2, lty = 2)
